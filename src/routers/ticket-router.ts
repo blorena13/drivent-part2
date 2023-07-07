52ce354b1327e06aad77e7cd540a5ae39789cb2a
Lorena <blorena1321@gmail.com>
2023-07-07 20:10:43 -0300

    feat: arrumando erros autenticação

--- src/routers/ticket-router.ts
+++ src/routers/ticket-router.ts
@@ -4,8 +4,9 @@ import ticketsController from "../controllers/tickets-controller";
 
 const ticketsRouter = Router();
 
-ticketsRouter.get("/tickets/types", authenticateToken, ticketsController.getTicketsTypes);
-ticketsRouter.get("/tickets", authenticateToken, ticketsController.getTicketsUser);
-ticketsRouter.post("/tickets", authenticateToken, ticketsController.createTicket);
+ticketsRouter
+.get("/tickets/types", authenticateToken, ticketsController.getTicketsTypes)
+.get("/tickets", authenticateToken, ticketsController.getTicketsUser)
+.post("/tickets", authenticateToken, ticketsController.createTicket)
 
 export { ticketsRouter };
--- src/services/ticket-service/index.ts
+++ src/services/ticket-service/index.ts
@@ -1,32 +1,34 @@
-import { notFoundError, requestError } from "../../errors";
-import ticketRepository from "../../repositories/tickets-repository";
+import { notFoundError, requestError } from '../../errors';
+import ticketRepository from '../../repositories/tickets-repository';
+import userRepository from '../../repositories/user-repository';
 
-export async function getTicketsTypes(){
-return await ticketRepository.getByTypes();
+export async function getTicketsTypes() {
+  return await ticketRepository.getByTypes();
 }
 
-export async function getTicktesUser(id: number){
-const ticketsUser = await ticketRepository.getTicketsUser(id);
-if (!ticketsUser){
+export async function getTicktesUser(id: number) {
+  const existsUser = await userRepository.findById(id);
+  if (!existsUser) throw notFoundError();
+
+  const ticketsUser = await ticketRepository.getTicketsUser(id);
+  if (!ticketsUser) {
     throw notFoundError();
+  }
+
+  return ticketsUser;
 }
 
-return ticketsUser;
-}
-
-export async function createTicket(ticketTypeId: number, enrollmentId: number){
-
-    if(!ticketTypeId){
-        throw requestError(400, 'falta ticketType na requisição');
-    }
-    return await ticketRepository.createTicket(ticketTypeId, enrollmentId);
-
+export async function createTicket(ticketTypeId: number, enrollmentId: number) {
+  if (!ticketTypeId) {
+    throw requestError(400, 'falta ticketType na requisição');
+  }
+  return await ticketRepository.createTicket(ticketTypeId, enrollmentId);
 }
 
 const ticketService = {
-    getTicketsTypes,
-    getTicktesUser,
-    createTicket
-}
+  getTicketsTypes,
+  getTicktesUser,
+  createTicket,
+};
 
 export default ticketService;
